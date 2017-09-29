68a7cf5c1cf1f30b4cff15f66c6ac870124f9b1f
saanmin <kimsun94@icloud.com>
2017-09-29 12:01:38 +0900

    연습

--- src/c0922/aya.java
+++ src/c0922/aya.java
@@ -1,4 +1,35 @@
 package c0922;
 
+import java.util.Random;
+
 public class aya {
+
+    static public void main (String[] arg) {
+/*
+        Random killer = new Random();
+        System.out.println(killer.nextInt());
+
+        System.out.println(killer.nextInt());
+        System.out.println("내가짱이다");
+        System.out.println("\u263A");
+
+        int ididmath = 9; //우선 변수의 타입을 지정해주고
+        //ididmath = 5 + 4; //변수에 값 지정
+        System.out.println(ididmath); //출력해!!
+
+        String johnsnow; //존스노우의 변수타입은 스트링!!
+        johnsnow = "Winter is Coming";
+        System.out.println(johnsnow);
+
+        System.out.println(Math.sqrt(9));
+*/
+        double lol;
+        lol = Math.sqrt ( 2 );
+        System.out.println(lol);
+
+        System.out.println(3.2 + 'J');
+
+    }
+
+
 }
---
+++ src/정리/ch1.java
@@ -0,0 +1,9 @@
+package 정리;
+
+public class ch1 {
+
+    static public void main (String[] arg) { //이것은 프로그램 시작 메소드
+
+        System.out.println("내가짱이다"); //프린트하는 메소드
+    }
+}
